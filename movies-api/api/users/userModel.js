5e25dc854219ec8d361cdd76f88b7d9e01692009
Teek Leng Ng <jeff2406n@gmail.com>
2020-12-24 02:53:47 +0000

    Topic 10 - Session based Auth

--- movies-api/api/users/userModel.js
+++ movies-api/api/users/userModel.js
@@ -13,4 +13,18 @@ const UserSchema = new Schema({
   favourites: [MovieSchema]
 });
 
+//find a user by username
+UserSchema.statics.findByUserName = function (username) {
+  return this.findOne({ username: username });
+};
+
+//simple password comparison
+UserSchema.methods.comparePassword = function (candidatePassword) {
+  const isMatch = this.password === candidatePassword;
+  if (!isMatch) {
+    throw new Error('Password mismatch');
+  }
+  return this;
+};
+
 export default mongoose.model('User', UserSchema);
