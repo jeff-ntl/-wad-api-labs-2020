d8c5ca4ee9ca16e624b6cd9f966b8438c84638c1
Teek Leng Ng <jeff2406n@gmail.com>
2020-12-24 13:48:24 +0000

    Topic 10 - Exercises

--- movies-api/api/users/index.js
+++ movies-api/api/users/index.js
@@ -20,11 +20,21 @@ router.post('/', async (req, res, next) => {
     });
   }
   if (req.query.action === 'register') {
-    await User.create(req.body).catch(next);
-    res.status(201).json({
+    if (/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{5,}$/.test(req.body.password))
+    {
+      await User.create(req.body).catch(next);
+      res.status(201).json({
       code: 201,
       msg: 'Successful created new user.',
-    });
+      });
+    }
+    else{
+      res.status(401).json({
+        success: false,
+        msg: 'Bad password.',
+      });
+    }
+    
   } else {
     const user = await User.findByUserName(req.body.username).catch(next);
       if (!user) return res.status(401).json({ code: 401, msg: 'Authentication failed. User not found.' });
@@ -68,13 +78,26 @@ router.get('/:userName/favourites', (req, res, next) => {
 
 //Add a favourite. No Error Handling Yet. Can add duplicates too!
 router.post('/:userName/favourites', async (req, res, next) => {
-  const newFavourite = req.body.id;
-  const userName = req.params.userName;
-  const movie = await movieModel.findByMovieDBId(newFavourite);
-  const user = await User.findByUserName(userName);
-  await user.favourites.push(movie._id);
-  await user.save(); 
-  res.status(201).json(user); 
+  try{
+    const newFavourite = req.body.id;
+    const userName = req.params.userName;
+    const movie = await movieModel.findByMovieDBId(newFavourite);
+    const user = await User.findByUserName(userName)
+    //console.log(user.favourites)
+    //console.log(movie._id)
+    if (user.favourites.indexOf(movie._id) == -1) {
+      await user.favourites.push(movie._id);
+      await user.save(); 
+      res.status(201).json(user);
+    }else{
+      res.status(401).json({
+      success: false,
+      msg: 'Duplicated movie entries.',
+      })
+    }
+  }catch(error){
+    next(error)
+  }
 });
 
 export default router;
